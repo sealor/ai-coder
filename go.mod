900bf6dc7d7d1109f520ce797d4458b11ab474a8
Stefan Richter <sealor@users.noreply.github.com>
2025-10-31 13:37:44 +0100

    Migrate to streaming

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/sealor/ai-coder
 go 1.25.1
 
 require (
-	github.com/openai/openai-go/v3 v3.7.0 // indirect
+	github.com/openai/openai-go/v3 v3.7.0
 	github.com/tidwall/gjson v1.14.4 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"os"
 
-	"github.com/openai/openai-go/packages/ssestream"
 	"github.com/openai/openai-go/v3"
 	"github.com/openai/openai-go/v3/option"
+	"github.com/openai/openai-go/v3/packages/ssestream"
 )
 
 func GetEnv(name, fallback string) string {
@@ -23,11 +24,12 @@ func main() {
 
 	client := openai.NewClient(
 		option.WithBaseURL(GetEnv("OLLAMA_URL", "http://nixos.lan:11434/v1")),
-		// option.WithDebugLog(nil)
+		// option.WithDebugLog(nil),
 	)
 
 	param := openai.ChatCompletionNewParams{
-		Model: "qwen3:1.7b",
+		Model:           "qwen3:1.7b",
+		ReasoningEffort: "",
 		Messages: []openai.ChatCompletionMessageParamUnion{
 			openai.SystemMessage("Answer in exactly one short sentence!"),
 		},
@@ -35,10 +37,50 @@ func main() {
 
 	param.Messages = append(param.Messages, openai.UserMessage("Say this is a test"))
 
-	completion, err := client.Chat.Completions.New(context.TODO(), param)
+	stream := client.Chat.Completions.NewStreaming(context.TODO(), param)
+
+	_, err := run(stream)
 	if err != nil {
-		panic(err.Error())
+		panic(err)
+	}
+}
+
+func run(stream *ssestream.Stream[openai.ChatCompletionChunk]) (openai.ChatCompletionAccumulator, error) {
+	acc := openai.ChatCompletionAccumulator{}
+
+	for stream.Next() {
+		chunk := stream.Current()
+		acc.AddChunk(chunk)
+
+		if content, ok := acc.JustFinishedContent(); ok {
+			println("Content stream finished:", content)
+		}
+
+		if tool, ok := acc.JustFinishedToolCall(); ok {
+			println("Tool call stream finished:", tool.Index, tool.Name, tool.Arguments)
+		}
+
+		if refusal, ok := acc.JustFinishedRefusal(); ok {
+			println("Refusal stream finished:", refusal)
+		}
+
+		if len(chunk.Choices) > 0 {
+			choice := chunk.Choices[0]
+
+			reasoningJSON, ok := choice.Delta.JSON.ExtraFields["reasoning"]
+			var reasoning string
+			if ok {
+				json.Unmarshal([]byte(reasoningJSON.Raw()), &reasoning)
+			}
+			if len(reasoning) > 0 {
+				print(reasoning)
+			}
+
+			if len(choice.Delta.Content) > 0 {
+				print(choice.Delta.Content)
+			}
+		}
 	}
 
-	println(completion.Choices[0].Message.Content)
+	return acc, stream.Err()
 }
